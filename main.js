80df2e8c57164be2b4c2653ef330a770c70759e6
Yuvraj Singh <singhyuvraj2k@gmail.com>
2024-03-16 16:01:51 +0530

    adding cashing

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
+
 (async () => {
-  const req = await fetch('https://valorant-api.com/v1/weapons');
-  const res = await req.json();
-  const data = res["data"];
+
+  async function getdata(fetchUrl) {
+    const req = await fetch(fetchUrl);
+    const res = await req.json();
+    return res;
+  }
+
+  const versionJson = await getdata('https://valorant-api.com/v1/version');
+  let versionData = versionJson["data"];
+  let weaponsData = [];
   const cardDiv = document.getElementById("card-div");
 
+  let localVersion = JSON.parse(localStorage.getItem("version"));
+  let loacalWeaponsData = JSON.parse(localStorage.getItem("weapons-data"));
+
+  // console.log(localVersion, versionData["version"], loacalWeaponsData);
+
+  if(localVersion && localVersion["version"] === versionData["version"]) {
+    weaponsData = loacalWeaponsData;
+  } else {
+    localStorage.setItem("version", JSON.stringify(versionData));
+    const weaponsJson = await getdata('https://valorant-api.com/v1/weapons');
+    weaponsData = weaponsJson["data"];
+    localStorage.setItem("weapons-data", JSON.stringify(weaponsData));
+  }
 
 
-  data.forEach(ele => {
+  weaponsData.forEach(ele => {
     cardDiv.innerHTML += `
     
     <div class="col">
     <div class="card h-100">
       <img class="m-4 img-fluid" src= ${ele["displayIcon"]}
-        alt=${ele["displayName"]} />
+        alt=${ele["displayName"]} loading="lazy"/>
       <div class="card-body">
         <h5 class="card-title">${ele["displayName"]}</h5>
         <p class="card-text">
